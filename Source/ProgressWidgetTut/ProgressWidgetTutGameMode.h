486f16bb0dc542219650e295ace90746b85ad5b3
Ben Reeves <benwolverine2019@gmail.com>
2015-11-18 15:50:07 -0500

    Initial Commit

---
+++ Source/ProgressWidgetTut/ProgressWidgetTutGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProgressWidgetTutGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRESSWIDGETTUT_API AProgressWidgetTutGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
